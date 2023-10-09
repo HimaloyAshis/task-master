2930f644a45262fd49867963b7f2ddad59fb9287
himaloyash <himaloyashis98@gmail.com>
2023-10-09 15:52:50 +0600

    firebase added

---
+++ src/utils/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkwNHa75A4adQ3A3WWDtfNE8lCarTq3x8",
+  authDomain: "task-master-b2c09.firebaseapp.com",
+  projectId: "task-master-b2c09",
+  storageBucket: "task-master-b2c09.appspot.com",
+  messagingSenderId: "885911197133",
+  appId: "1:885911197133:web:2da1684434f298a7ee6084"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
