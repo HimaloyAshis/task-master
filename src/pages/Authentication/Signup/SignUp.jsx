b619a5d488e6cd4906d6e82fefe61cef0118d2fb
himaloyash <himaloyashis98@gmail.com>
2023-10-19 23:26:53 +0600

    sign up

--- src/pages/Authentication/Signup/SignUp.jsx
+++ src/pages/Authentication/Signup/SignUp.jsx
@@ -11,26 +11,28 @@ const SignUp = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label>Name</label>
-                    <input {...register("name")} />
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input {...register("email")} />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input {...register("password")} />
-                </div>
-                <div>
-                    <label>Confirm Password</label>
-                    <input {...register("confirm")} />
-                </div>
-                <input type="submit" />
-            </form>
+        <div className='bg-[#232D3F]'>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label>Name</label>
+                        <input {...register("name")} />
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input {...register("email")} />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input {...register("password")} />
+                    </div>
+                    <div>
+                        <label>Confirm Password</label>
+                        <input {...register("confirm")} />
+                    </div>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
